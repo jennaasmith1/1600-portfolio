79495fbb31004ca005b81d4d7ea2d6e11ac3c4df
Jenna Smith <jennaasmith1@gmail.com>
2020-12-17 11:43:53 -0700

    fix star wars films

--- starWarsFilms/main.js
+++ starWarsFilms/main.js
@@ -5,7 +5,11 @@ const main = document.querySelector('main')
 for (let i = 0; i < 7; i++) {
     let figure = document.createElement('figure')
     let figImg = document.createElement('img')
-    figImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
+
+    const urlSplit = films[i].url.split('/')
+    const id = urlSplit[urlSplit.length - 2]
+    figImg.src = `https://starwars-visualguide.com/assets/img/films/${id}.jpg`
+
     let figCaption = document.createElement('figcaption')
     figCaption.textContent = films[i].title
 
