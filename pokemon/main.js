a1bae67b4c7fa61cca241cf9c3b60c0ee87edcb4
Jenna Smith <jennaasmith1@gmail.com>
2020-12-16 21:29:47 -0700

    deleted comments

--- pokemon/main.js
+++ pokemon/main.js
@@ -110,20 +110,3 @@ async function main() {
 }
 
 main()
-//const main = document.querySelector('main')
-
-
-//main.appendChild(card)
-
-
-// for   (let i = 0; i < 7; i++) {
-//     let figure = document.createElement('figure')
-//     let figImg = document.createElement('img')
-//     figImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
-//     let figCaption = document.createElement('figcaption')
-//     figCaption.textContent = films[i].title
-
-//     figure.appendChild(figImg)
-//     figure.appendChild(figCaption)
-//     main.appendChild(figure)
-// }
